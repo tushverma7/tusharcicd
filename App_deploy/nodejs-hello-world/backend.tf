42a6e1be210cb7cb92b7beabc825425c83f0a0c4
Tushar <tushverma7@gmail.com>
2025-04-08 15:30:20 +0530

    Fix issues

--- App_deploy/nodejs-hello-world/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "azurerm" {
-    resource_group_name  = "TestEnv"
-    storage_account_name = "poclabtest001"
-    container_name       = "testcon"
-    key                  = "terraform.tfstate"
-  }
-}
